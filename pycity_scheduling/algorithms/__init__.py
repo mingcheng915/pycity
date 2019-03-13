0cbb08e1ed5a3a70cce1f1608bce41839c6200da
Sebastian Uerlich <sebastian.uerlich@rwth-aachen.de>
2019-03-13 18:21:14 +0100

    WIP add mpi support for exchange_admm

--- pycity_scheduling/algorithms/__init__.py
+++ pycity_scheduling/algorithms/__init__.py
@@ -3,11 +3,13 @@ from .local_optimization_algorithm import local_optimization
 from .exchange_admm_algorithm import exchange_admm
 from .central_optimization_algortihm import central_optimization
 from .dual_decomposition_algortihm import dual_decomposition
+from .exchange_admm_algorithm_mpi import exchange_admm_mpi
 
 algorithms = {
     "stand-alone": stand_alone_optimization,
     "local": local_optimization,
     "exchange-admm": exchange_admm,
+    "exchange-admm-mpi": exchange_admm_mpi,
     "central": central_optimization,
     "dual-decomposition": dual_decomposition,
 }
@@ -16,6 +18,7 @@ __all__ = [
     "stand_alone_optimization",
     "local_optimization",
     "exchange_admm",
+    "exchange_admm_mpi",
     "central_optimization",
     "dual_decomposition",
     "algorithms",
---
+++ pycity_scheduling/util/mpi_optimize_nodes.py
@@ -0,0 +1,114 @@
+import mpi4py as MPI
+from pycity_scheduling.classes import *
+from pycity_scheduling.exception import *
+import numpy as np
+import gurobipy as gurobi
+import sys
+from typing import Iterable
+
+class Node(dict):
+    def __init__(self, worker: int, node):
+        super(Node, self).__init__(node)
+        self.__worker_id__ = worker
+        self.__idata__ = None
+    def do_iteration(self,quad_obj, lin_obj, timeLimit=60):
+        self.__i_data__ = {'node_id': node.name, 'timeLimit': timeLimit, 'obj_mod':[quad_obj, lin_obj]}
+
+class MPI_Nodes(dict):
+    def __init__(self, comm, nodes):
+        super(MPI_Nodes, self).__init__()
+        self.__comm__ = comm
+        self.__size__ = comm.Get_remote_size()
+        self.__workers__ = range(self.__size__)
+        for node_i, node in enumerate(nodes):
+            self[node.name] = Node(node_i % self.__size__, node)
+        for worker in self.__workers__:
+            comm.send([node for node in self.values() if node.__worker_id__ == worker], dest=worker, tag=10)
+
+    def calculate(self):
+        #send work out
+        for worker_id in range(self.__size__):
+            comm.send([node.__i_data__ for node in self.values() if
+                       node.__i_data__ is not None and node.__worker_id__ == worker_id]
+                      , dest=worker_id, tag=11)
+        #reset i_data
+        for node in self.values():
+            node.__i_data__ = None
+        #collect work
+        for worker_id in range(self.__size__):
+            data = comm.recv(source=worker_id, tag=12)
+            for name, p in data.items():
+                self[name]['P_El_Schedule'] = p
+        return
+
+
+class mpi_context:
+    def __enter__(self, nodes, max_procs=10):
+        self.comm = MPI.COMM_SELF.Spawn(sys.executable, args=['mpi_optimize_nodes.py'], maxprocs=max_procs)
+        self.size = comm.Get_remote_size()
+        return MPI_Nodes(comm, nodes)
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        for i in range(size):
+            comm.send(None, dest=i, tag=11)
+        comm.Disconnect()
+
+
+
+
+
+if __name__ == '__main__':
+
+    comm = MPI.Comm.Get_parent()
+    size = comm.Get_size()
+    rank = comm.Get_rank()
+
+    nodes = comm.recv(source=0, tag=10)
+    for node in nodes:
+        m = gurobi.Model("single node")
+        m.setParam("LogFile", "")
+        m.setParam("Threads", 1)
+        node["model"] = m
+    i_data = comm.recv(source=0, tag=11)
+    while i_data is not None:
+        output = {}
+        for node_data in i_data:
+            if node_data is None:
+                continue
+            node_id = node_data["node_id"]
+            node = nodes[node_id]
+            entity = node['entity']
+            obj_mod = node_data['obj_mod']
+            m = node["model"]
+            m.setParam("TimeLimit", node_data['timeLimit'])
+            if not isinstance(
+                    entity,
+                    (Building, Photovoltaic, WindEnergyConverter)
+            ):
+                continue
+            obj = entity.get_objective()
+            # penalty term is expanded and constant is omitted
+            obj.addTerms(
+                obj_mod[0],
+                entity.P_El_vars,
+                entity.P_El_vars
+            )
+            obj.addTerms(
+                obj_mod[1],
+                entity.P_El_vars
+            )
+
+            m.setObjective(obj)
+            m.optimize()
+
+            try:
+                output[node.name] = [var.x for var in entity.P_El_vars]
+            except gurobi.GurobiError:
+                print("Model Status: %i" % m.status)
+                raise PyCitySchedulingGurobiException(
+                    "{0}: Could not read from variables."
+                        .format(str(node))
+                )
+        comm.send(output, dest=0, tag=12)
+        i_data = comm.recv(source=0, tag=11)
+    comm.Disconnect()
